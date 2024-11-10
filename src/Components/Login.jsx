c48a1e0ee12046ff484d56d76b921ac7b6221b1e
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2024-11-10 18:11:23 +0600

    final push and complete

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,20 +1,66 @@
-import React from 'react';
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../Firebase/firebase.init';
 
 
 const Login = () => {
+    const [errorMessage, setErrorMessage] = useState('')
+    const [success, setSuccess] = useState(false)
+
+    const emailRef = useRef()
+
+    const handleLogin = e => {
+        e.preventDefault()
+
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        setErrorMessage('')
+        setSuccess(false)
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                if (!result.user.emailVerified) {
+                    setErrorMessage("Please Verify your email first")
+                }
+                else {
+                    setSuccess(true)
+                }
+
+            })
+            .catch(error => {
+                setErrorMessage(error.message)
+            })
+    }
+
+    const handleForgotPassword = () => {
+
+        const email = emailRef.current.value
+        if (!email) {
+            setErrorMessage("Enter Valid Email Address")
+
+        }
+        else {
+            sendPasswordResetEmail(auth, email)
+                .then(result => {
+                    alert("Eamil send . Please Reset Password")
+                })
+                .catch(error => setErrorMessage(error.message))
+        }
+    }
     return (
         <>
             <div className="hero bg-base-200 min-h-screen">
                 <div className="hero-content   rounded-lg flex-col lg:flex-row-reverse">
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                         <h1 className='text-center font-bold text-2xl pt-3 text-blue-700'>Login</h1>
-                        <form className="card-body">
+                        <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" name='email' placeholder="Email" className="input input-bordered" required />
+                                <input type="email" ref={emailRef} name='email' placeholder="Email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
@@ -22,15 +68,21 @@ const Login = () => {
                                 </label>
                                 <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                                 <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    <a href="#" onClick={handleForgotPassword} className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
                             </div>
                         </form>
-                       
+                        {
+                            success && <p className='text-center font-bold text-green-400 pb-2 px-5'>Login Successfull</p>
+                        }
+                        {
+                            errorMessage && <p className='font-bold text-center text-red-400 pb-2 px-5'>{errorMessage}</p>
+                        }
                         <p className='text-center pb-4'>Don't have account? <Link className='text-blue-700 ml-3 font-bold' to='/signup'>Register</Link></p>
+
                     </div>
                 </div>
             </div>
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { FcGoogle } from "react-icons/fc";
 import { VscGithub } from "react-icons/vsc";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { createUserWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider, sendEmailVerification, signInWithPopup } from 'firebase/auth';
 import { auth } from '../Firebase/firebase.init';
 const Signup = () => {
     const [success, setSuccess] = useState(false)
@@ -42,9 +42,11 @@ const Signup = () => {
         const email = e.target.email.value
         const name = e.target.name.value
         const password = e.target.password.value
+        const terms = e.target.checkbox.checked
 
         setSuccess(false)
         setErrorMessage('')
+
 
         if (password.length < 6) {
             setErrorMessage('Password Must Contain 6 characters')
@@ -56,10 +58,18 @@ const Signup = () => {
             setErrorMessage('Password must contain upper case,lower case, digit and special characters.')
             return
         }
+        if (!terms) {
+            setErrorMessage("Accept terms and conditions")
+            return
+        }
 
         createUserWithEmailAndPassword(auth, email, password)
             .then(result => {
                 setSuccess(true)
+                // email verification
+                sendEmailVerification(auth.currentUser)
+                    .then()
+                    .catch()
             })
             .catch(error => {
                 console.log(error);
@@ -91,27 +101,34 @@ const Signup = () => {
                                 </label>
                                 <input type={showPassword ? 'text' : "password"} name='password' placeholder="password" className="input input-bordered" required />
 
-                                <button onClick={() => setShowPassword(!showPassword)} className='text-lg absolute top-12 right-4'>
+                                <button type="button" onClick={() => setShowPassword(!showPassword)} className='text-lg absolute top-12 right-4'>
                                     {showPassword ? <FaEyeSlash /> : <FaEye />}
                                 </button>
+                            </div>
+                            <div className="form-control">
+                                <label className="cursor-pointer justify-start label">
+                                    <input type="checkbox" name='checkbox' className="checkbox checkbox-success" />
+                                    <span className="label-text text-blue-400 font-bold text-lg ml-3">Accept terms and conditions</span>
 
+                                </label>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Sign Up</button>
                             </div>
                         </form>
+                        {
+                            success && <p className='font-bold text-center text-xl text-green-400 pb-5'>Sign Up successful</p>
+                        }
+                        {
+                            errorMessage && <p className='font-bold text-center text-red-500 pb-5'>{errorMessage}</p>
+                        }
                         <div className='flex gap-3 px-4 mb-5'>
                             <button onClick={handleGoogleLogin} className='btn btn-primary'>Sign Up With <span className='text-xl text-white'><FcGoogle /></span></button>
                             <button onClick={handleGithubLogin} className='btn btn-primary'>Sign Up With <span className='text-xl text-purple-400'><VscGithub /></span></button>
                         </div>
                         <p className='text-center pb-3'>Already have account? <Link className='text-blue-700 ml-3 font-bold' to='/login'>Login</Link></p>
 
-                        {
-                            success && <p className='text-center text-xl text-green-400 pb-3'>Sign Up successful</p>
-                        }
-                        {
-                            errorMessage && <p className='text-center text-red-500 pb-2'>{errorMessage}</p>
-                        }
+
                     </div>
                 </div>
             </div>
